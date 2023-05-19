20b882a14daa03ba47abc74849bffa087bc8781b
spotich <popenko1337@gmail.com>
2023-05-19 14:03:01 +0700

    slice header playground

--- main.go
+++ main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"reflect"
+	"unsafe"
 )
 
 func main() {
-	fmt.Println("My BioCAD project.")
-	time.Sleep(10 * time.Second)
+	sl := make([]int, 0)
+
+	for i := 1; i <= 5; i++ {
+		sl = append(sl, i)
+	}
+	sh := (*reflect.SliceHeader)(unsafe.Pointer(&sl))
+	fmt.Println("mem region = ", sh.Data, "\tpointer = ", &sh.Data)
+
+	fmt.Println("foo exec")
+	foo(sl)
+}
+
+func foo(sl []int) {
+	sl[4] = 10
+	sb := (*reflect.SliceHeader)(unsafe.Pointer(&sl))
+	fmt.Println("mem region = ", sb.Data, "\tpointer = ", &sb.Data)
 }
